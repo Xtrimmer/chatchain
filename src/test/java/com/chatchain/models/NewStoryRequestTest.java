6c232f0bb71ea723b75a92596d42dafa1b365692
Jeff Trimmer <trimmerjeff@gmail.com>
2018-08-02 15:04:46 -0700

    Added newStoryRequest unit tests.

---
+++ src/test/java/com/chatchain/models/NewStoryRequestTest.java
@@ -0,0 +1,73 @@
+package com.chatchain.models;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class NewStoryRequestTest
+{
+    private NewStoryRequest testRequest;
+
+    @BeforeEach
+    void setUp()
+    {
+        testRequest = new NewStoryRequest();
+    }
+
+    @Test
+    void getTitle()
+    {
+        String expectedTitle = "Test Title";
+        testRequest.setTitle(expectedTitle);
+        assertEquals(expectedTitle, testRequest.getTitle());
+    }
+
+    @Test
+    void setTitle()
+    {
+        String expectedTitle = "Another Test Title";
+        testRequest.setTitle(expectedTitle);
+        assertEquals(expectedTitle, testRequest.getTitle());
+
+        expectedTitle = "Test title that has a length of 50 characters long";
+        testRequest.setTitle("Test title that has a length of 50 characters long plus extra characters");
+        assertEquals(expectedTitle, testRequest.getTitle());
+    }
+
+    @Test
+    void getCitation()
+    {
+        String expectedCitation = "Test citation";
+        testRequest.setCitation(expectedCitation);
+        assertEquals(expectedCitation, testRequest.getCitation());
+    }
+
+    @Test
+    void setCitation()
+    {
+        String expectedCitation = "Another Test citation";
+        testRequest.setCitation(expectedCitation);
+        assertEquals(expectedCitation, testRequest.getCitation());
+
+        expectedCitation = "A test citation that has a length of 50 characters";
+        testRequest.setCitation("A test citation that has a length of 50 characters plus extra characters");
+        assertEquals(expectedCitation, testRequest.getCitation());
+    }
+
+    @Test
+    void getPeriod()
+    {
+        int expectedPeriod = 10;
+        testRequest.setPeriod(expectedPeriod);
+        assertEquals(expectedPeriod, testRequest.getPeriod());
+    }
+
+    @Test
+    void setPeriod()
+    {
+        int expectedPeriod = 20;
+        testRequest.setPeriod(expectedPeriod);
+        assertEquals(expectedPeriod, testRequest.getPeriod());
+    }
+}
