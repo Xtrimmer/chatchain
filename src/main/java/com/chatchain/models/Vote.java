50cc3f799c60d0b60c40c607321d6fce1b99df9c
Jeff Trimmer <trimmerjeff@gmail.com>
2018-08-03 07:16:46 -0700

    Moved enum to its own class

--- src/main/java/com/chatchain/models/Vote.java
+++ src/main/java/com/chatchain/models/Vote.java
@@ -2,23 +2,6 @@ package com.chatchain.models;
 
 import org.apache.commons.lang3.builder.ToStringBuilder;
 
-enum VoteType
-{
-    UPVOTE(1), DOWNVOTE(-1);
-
-    private final int value;
-
-    VoteType(int value)
-    {
-        this.value = value;
-    }
-
-    public int getValue()
-    {
-        return value;
-    }
-}
-
 public class Vote
 {
     private String phrase;
---
+++ src/main/java/com/chatchain/models/VoteType.java
@@ -0,0 +1,18 @@
+package com.chatchain.models;
+
+public enum VoteType
+{
+    UPVOTE(1), DOWNVOTE(-1);
+
+    private final int value;
+
+    VoteType(int value)
+    {
+        this.value = value;
+    }
+
+    public int getValue()
+    {
+        return value;
+    }
+}
