dc30e00b7bbb51aa355de867fa4f6d13961aa62b
Jeff Trimmer <trimmerjeff@gmail.com>
2018-08-20 12:40:28 -0700

    Added payserver configuration

---
+++ src/main/java/com/chatchain/configurations/PaymentServerConfiguration.java
@@ -0,0 +1,29 @@
+package com.chatchain.configurations;
+
+import com.chatchain.services.PaymentRequestService;
+import feign.Feign;
+import feign.jackson.JacksonDecoder;
+import feign.jackson.JacksonEncoder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class PaymentServerConfiguration
+{
+    @Value("${payserver.rootUrl}")
+    private String payServerRootUrl;
+
+    @Value("${payserver.authentication}")
+    private String authHeader;
+
+    @Bean
+    public PaymentRequestService.BtcPayServer getPaymentServer()
+    {
+        return Feign.builder()
+                .encoder(new JacksonEncoder())
+                .decoder(new JacksonDecoder())
+                .requestInterceptor(request -> request.header("Authorization", authHeader))
+                .target(PaymentRequestService.BtcPayServer.class, payServerRootUrl);
+    }
+}
--- src/main/java/com/chatchain/services/PaymentRequestService.java
+++ src/main/java/com/chatchain/services/PaymentRequestService.java
@@ -4,11 +4,8 @@ import com.chatchain.models.CreateInvoiceResponse;
 import com.chatchain.models.Invoice;
 import com.chatchain.models.InvoiceUrl;
 import com.chatchain.models.PaidRequest;
-import feign.Feign;
 import feign.Headers;
 import feign.RequestLine;
-import feign.jackson.JacksonDecoder;
-import feign.jackson.JacksonEncoder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,20 +17,18 @@ public class PaymentRequestService
 {
     private Map<String, PaidRequest> paidRequestMap = new HashMap<>();
     private StoryManagementService storyManagementService;
+    private BtcPayServer btcPayServer;
 
     @Autowired
-    public PaymentRequestService(StoryManagementService storyManagementService)
+    public PaymentRequestService(StoryManagementService storyManagementService,
+                                 BtcPayServer btcPayServer)
     {
         this.storyManagementService = storyManagementService;
+        this.btcPayServer = btcPayServer;
     }
 
-    interface BtcPayServer
+    public interface BtcPayServer
     {
-        BtcPayServer api = Feign.builder()
-                .encoder(new JacksonEncoder())
-                .decoder(new JacksonDecoder())
-                .target(BtcPayServer.class, "http://127.0.0.1:50008");
-
         @RequestLine("POST /invoices")
         @Headers({"Content-Type: application/json", "Authorization: Basic ZEg3SGtNVVFsMW9JUU9ycmwwUmhrN1liN1prbG9WYlpRUXlacFdoVjdkVQ=="})
         CreateInvoiceResponse createInvoice(Invoice invoice);
@@ -42,7 +37,7 @@ public class PaymentRequestService
     public InvoiceUrl addRequest(PaidRequest paidRequest)
     {
         Invoice invoice = paidRequest.getInvoice(storyManagementService);
-        CreateInvoiceResponse response = BtcPayServer.api.createInvoice(invoice);
+        CreateInvoiceResponse response = btcPayServer.createInvoice(invoice);
         String invoiceId = (String) response.getData().get("id");
         paidRequestMap.put(invoiceId, paidRequest);
         return new InvoiceUrl((String) response.getData().get("url"));
