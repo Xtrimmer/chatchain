6132a8f7cb6e244d2dea8ceddac60eb209540deb
Jeff Trimmer <trimmerjeff@gmail.com>
2018-08-30 09:03:20 -0700

    Removed unneeded region variable.

--- src/main/java/com/chatchain/configurations/AwsConfiguration.java
+++ src/main/java/com/chatchain/configurations/AwsConfiguration.java
@@ -3,16 +3,12 @@ package com.chatchain.configurations;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AwsConfiguration
 {
-    @Value("${amazon.dynamodb.region}")
-    private String amazonAWSRegion;
-
     @Bean
     public AmazonDynamoDB amazonDynamoDB()
     {
